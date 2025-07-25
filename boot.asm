f442e2dd2768a3f0aa1a8bd7c9b768bc9b9c2548
Manu Sankar <156907586+manu-sankar@users.noreply.github.com>
2025-07-26 03:05:39 +0530

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,3 +1,4 @@
+/*
 [bits 16]
 [org 0x7c00]
 
@@ -22,4 +23,48 @@ main:
     hlt
 
 times 510 - ($ - $$) db 0
+dw 0xAA55
+*/
+
+
+[bits 16]
+[org 0x7c00]
+start:
+    cli
+    lgdt [gdtr]    ; Load GDT
+    mov eax, cr0
+    or al, 1       ; Enable protected mode
+    mov cr0, eax
+    jmp 0x08:protected_mode
+
+[bits 32]
+protected_mode:
+    mov ax, 0x10   ; Setup segments
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    jmp 0x7e00     ; Jump to kernel
+
+gdtr:
+    dw 23          ; GDT limit
+    dd gdt         ; GDT address
+
+gdt:
+    dq 0           ; Null descriptor
+    dw 0xFFFF      ; Code segment
+    dw 0
+    db 0
+    db 0x9A
+    db 0xCF
+    db 0
+    dw 0xFFFF      ; Data segment
+    dw 0
+    db 0
+    db 0x92
+    db 0xCF
+    db 0
+
+times 510-($-$$) db 0
 dw 0xAA55
