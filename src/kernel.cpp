307066a9ab6f7b78968938afd57972dc4f6fbe21
Manu Sankar <156907586+manu-sankar@users.noreply.github.com>
2025-07-26 00:18:34 +0530

    Create kernel.cpp

---
+++ src/kernel.cpp
@@ -0,0 +1,15 @@
+extern "C" void kernel_main();
+
+extern "C" void kernel_main() {
+    const char *str = "Hello, 64-bit OS!";
+    char *vidptr = (char*) 0xb8000; // Video memory
+    unsigned int i = 0;
+
+    while (str[i] != '\0') {
+        vidptr[i * 2] = str[i]; // Character
+        vidptr[i * 2 + 1] = 0x07; // Attribute-byte: light grey on black
+        i++;
+    }
+
+    while (1); // Loop forever
+}
